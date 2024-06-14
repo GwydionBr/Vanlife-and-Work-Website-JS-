5328ccb2212789ae25cc3dfb3f6af22ae42af302
GwydionBr <155477872+GwydionBr@users.noreply.github.com>
2024-06-14 17:48:04 +0200

    feat: Add error page and update spot view
    This commit adds a new error page and updates the spot view to display a list of spots with their names, descriptions, and ratings. The error page is included in the views directory, and the spot view now uses a loop to render each spot dynamically. The spot view also includes a link to view each individual spot.
    
    The code changes include:
    - Addition of the error.ejs file in the views directory
    - Modification of the spots.ejs file to display a list of spots using a loop
    - Update of the addSpot.ejs file to include additional input fields for spot description, general rating, internet rating, location, start date, and end date
    - Modification of the index.js file to handle the rendering of the error page and the spot view

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 
+env.config();
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
@@ -12,9 +24,16 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/spots", (req, res) => {
-  res.render("spots.ejs");
-})
+app.get("/spots", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM spots");
+    console.log(result.rows);
+    res.render("spots.ejs", { spots: result.rows });
+  } catch (err) {
+    console.log(err);
+    res.render("error.ejs", { error: err }); // Assuming you have an error.ejs for error handling
+  }
+});
 
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
@@ -24,16 +43,24 @@ app.get("/about", (req, res) => {
   res.render("about.ejs");
 })
 
-app.get("/addspot", (req, res) => {
+app.get("/addSpot", (req, res) => {
   res.render("addSpot.ejs");
 })
 
-app.post("/check", (req, res) => {
-  res.render("spots.ejs", {
-    spotName: req.body.spotName,
-    rating: req.body.rating,
-  });
-})
+app.post("/addSpot", async (req, res) => {
+  const {spotName, description, generalRating, internetRating, location, start_date, end_date} = req.body;
+  try {
+    await db.query(
+      `INSERT INTO spots (name, description, general_rating, internet_rating, location, start_date, end_date) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7)`, 
+      [spotName, description, generalRating, internetRating, location, start_date, end_date]);
+    res.redirect("/spots");
+  } catch (err) {
+    console.log(err);
+    res.render("error.ejs", { error: err }); // Assuming you have an error.ejs for error handling
+  }
+}
+);
 
 
 app.listen(port, () => {
